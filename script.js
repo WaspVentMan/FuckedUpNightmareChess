060b926a929398bdc34843d7d7f3d71abf873ce3
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2024-04-27 21:40:46 +0100

    1.2a

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let board = [
+screenBoard = document.querySelector(".board")
+screenMenu = document.querySelector(".menu")
+
+let emptyBoard = [
     ["br", "bp", "", "", "", "", "wp", "wr"],
     ["bn", "bp", "", "", "", "", "wp", "wn"],
     ["bb", "bp", "", "", "", "", "wp", "wb"],
@@ -8,6 +11,8 @@ let board = [
     ["bn", "bp", "", "", "", "", "wp", "wn"],
     ["br", "bp", "", "", "", "", "wp", "wr"],
 ]
+
+let board = []
 
 let pieceIndex = {
     "r": "rook",
@@ -19,20 +24,68 @@ let pieceIndex = {
 }
 
 let select = [8, 8]
+let sTurn = false
 let turn = false
+let funny = false
+let cpu = {
+    "enabled": true,
+    "valid": [],
+    "validMove": []
+}
+
+function toggleTurn(){
+    sTurn = !sTurn
+
+    let turnText = document.querySelector(".turnToggle")
+
+    if (sTurn){
+        turnText.textContent = "Black Starts"
+    } else {
+        turnText.textContent = "White Starts"
+    }
+}
+
+function toggleCPU(){
+    cpu.enabled = !cpu.enabled
+
+    let cpuText = document.querySelector(".cpuToggle")
+
+    if (cpu.enabled){
+        cpuText.textContent = "YOU vs CPU"
+    } else {
+        cpuText.textContent = "YOU vs FRIEND"
+    }
+}
+
+function toggleFunny(){
+    funny = !funny
+
+    let funnyText = document.querySelector(".funnyToggle")
+
+    if (funny){
+        funnyText.textContent = "Enabled :)"
+    } else {
+        funnyText.textContent = "Disabled"
+    }
+}
+
+function resetBoard(){
+    turn = sTurn
+    board = JSON.parse(JSON.stringify(emptyBoard))
+}
 
 function posCheck(piece, x, y){
     if (piece[1] == undefined){
         return false
     }
 
-    if (board[x][y][0] == board[select[0]][select[1]][0]){
+    if (board[x][y][0] == piece[0]){
         return false
     }
 
     if (piece[1] == "p"){
         if (piece[0] == "b"){
-            if (select[1] == 1 && y - 2 == select[1] && board[x][y - 1] == "" && x == select[0]){
+            if (select[1] == 1 && y - 2 == select[1] && board[x][y - 1] == "" && board[x][y] == "" && x == select[0]){
                 return true
             }
             if (y - 1 == select[1] && x == select[0] && board[x][y][0] != "w"){
@@ -42,7 +95,7 @@ function posCheck(piece, x, y){
                 return true
             }
         } else if (piece[0] == "w"){
-            if (select[1] == 6 && y + 2 == select[1] && board[x][y + 1] == "" && x == select[0]){
+            if (select[1] == 6 && y + 2 == select[1] && board[x][y + 1] == "" && board[x][y] == "" && x == select[0]){
                 return true
             }
             if (y + 1 == select[1] && x == select[0] && board[x][y][0] != "b"){
@@ -152,7 +205,7 @@ function posCheck(piece, x, y){
                 x--
                 y--
 
-                while (x < select[0]){
+                while (x > select[0]){
                     if (board[x][y] != ""){
                         return false
                     }
@@ -278,7 +331,7 @@ function posCheck(piece, x, y){
                 x--
                 y--
 
-                while (x < select[0]){
+                while (x > select[0]){
                     if (board[x][y] != ""){
                         return false
                     }
@@ -327,8 +380,26 @@ function posCheck(piece, x, y){
     }
 }
 
-function constructor(){
-    document.body.innerHTML = ""
+function renderWinner(colour){
+    screenBoard.innerHTML = ""
+    screenBoard.style.display = "block"
+    let winCard = document.createElement("div")
+    winCard.innerHTML = `<p class="winText">${colour.toUpperCase()} WINS!</p><div class="winButtonDiv"><button class="winButton" onclick="resetBoard(); renderBoard()">Restart</button><button class="winButton" onclick="screenMenu.style.left = '16px'; screenBoard.innerHTML = ''">Menu</button></div>`
+    
+    winCard.className = "winCard"
+    winCard.style.color = colour
+    screenBoard.appendChild(winCard)
+
+    turn = false
+}
+
+function renderBoard(){
+    screenMenu.style.left = '-800px'
+    screenBoard.innerHTML = ""
+    screenBoard.style.display = "grid"
+
+    cpu.valid = []
+    cpu.validMove = []
 
     let winState = [true, true]
     let turnColour = "#000000AA"
@@ -383,21 +454,24 @@ function constructor(){
             }
 
             if (!check){
-                if ((turn && board[x][y][0] != "w") || (!turn && board[x][y][0] != "b"))
-                outerPiece.onclick = function(){
-                    if (board[x][y] != ""){
-                        select = [x, y]
-                        let audio = new Audio('audio/grab.mp3')
-                        audio.play()
-                    } else {
-                        select = [8, 8]
+                if ((turn && board[x][y][0] != "w" && !cpu.enabled) || (!turn && board[x][y][0] != "b")) {
+                    outerPiece.onclick = function(){
+                        if (board[x][y] != ""){
+                            select = [x, y]
+                            let audio = new Audio('audio/grab.mp3')
+                            audio.play()
+                        } else {
+                            select = [8, 8]
+                        }
+        
+                        renderBoard()
                     }
-    
-                    constructor()
+                } else if (turn && board[x][y][0] == "b" && cpu.enabled) {
+                    cpu.valid.push([x, y])
                 }
             } else {
                 outerPiece.onclick = function(){
-                    if (board[x][y] != "" && Math.random() > 0.999){
+                    if (board[x][y] != "" && funny){
                         let audio = new Audio('funny/luigiScream.mp3')
                         audio.play()
                     }
@@ -411,35 +485,128 @@ function constructor(){
                     let audio = new Audio('audio/place.mp3')
                     audio.play()
     
-                    constructor()
+                    renderBoard()
                 }
             }
 
             outerPiece.appendChild(innerPiece)
             containPiece.appendChild(outerPiece)
-            document.body.appendChild(containPiece)
+            screenBoard.appendChild(containPiece)
         }
     }
 
     console.log(winState)
 
     if (winState[0]){
-        document.body.innerHTML = ""
-        document.body.style.display = "block"
-        let winCard = document.createElement("div")
-        winCard.className = "winCard"
-        winCard.textContent = "BLACK WINS!"
-        winCard.style.color = "black"
-        document.body.appendChild(winCard)
+        renderWinner("black")
+        return
     } else if (winState[1]){
-        document.body.innerHTML = ""
-        document.body.style.display = "block"
-        let winCard = document.createElement("div")
-        winCard.className = "winCard"
-        winCard.textContent = "WHITE WINS!"
-        winCard.style.color = "white"
-        document.body.appendChild(winCard)
+        renderWinner("white")
+        return
+    }
+
+    if (turn && cpu.enabled) {
+        setTimeout(function(){
+            while (true){
+                let choice = Math.round(Math.random()*(cpu.valid.length-1))
+                console.log(choice)
+    
+                select = [cpu.valid[choice][0], cpu.valid[choice][1]]
+    
+                piece = board[select[0]][select[1]]
+    
+                for (let y = 0; y < 8; y++){
+                    for (let x = 0; x < 8; x++){
+                        if (posCheck(piece, x, y)){
+                            cpu.validMove.push([x, y])
+                        }
+                    }
+                }
+    
+                if (cpu.validMove.length == 0){
+                    continue
+                }
+
+                let audio = new Audio('audio/grab.mp3')
+                audio.play()
+
+                renderBoardLite()
+                break
+            }
+            setTimeout(function(){
+                choice = Math.round(Math.random()*(cpu.validMove.length-1))
+    
+                if (board[cpu.validMove[choice][0]][cpu.validMove[choice][1]] != "" && funny){
+                    let audio = new Audio('funny/luigiScream.mp3')
+                    audio.play()
+                } else {
+                    let audio = new Audio('audio/place.mp3')
+                    audio.play()
+                }
+    
+                board[select[0]][select[1]] = ""
+                board[cpu.validMove[choice][0]][cpu.validMove[choice][1]] = piece
+    
+                select = [8, 8]
+
+                turn = !turn
+                renderBoard()
+            }, (1000*Math.random())+500)
+        }, (1000*Math.random())+500)
     }
 }
 
-constructor()
+function renderBoardLite(){
+    screenMenu.style.left = '-800px'
+    screenBoard.innerHTML = ""
+    screenBoard.style.display = "grid"
+
+    let turnColour = "#000000AA"
+
+    if (turn){
+        turnColour = "#ffffffAA"
+    }
+
+    let piece = ""
+    
+    if (select[0] != 8){
+        piece = board[select[0]][select[1]]
+    }
+
+    for (let y = 0; y < 8; y++){
+        for (let x = 0; x < 8; x++){
+            let check = posCheck(piece, x, y)
+
+            let containPiece = document.createElement("div")
+            containPiece.className = "containPiece"
+
+            if ((select[0] == x && select[1] == y) || check){
+                containPiece.style.backgroundColor = turnColour
+            }
+
+            let innerPiece = document.createElement("div")
+            innerPiece.className = "innerPiece"
+
+            if (board[x][y] != ""){
+                innerPiece.style.backgroundImage = "url('pieces/" + pieceIndex[board[x][y][1]] + "_outline.png')"
+            }
+
+            innerPiece.style.filter = "invert()"
+
+            let outerPiece = document.createElement("div")
+            outerPiece.className = "outerPiece"
+
+            outerPiece.style.backgroundImage = "url('pieces/" + pieceIndex[board[x][y][1]] + ".png')"
+
+            if (board[x][y][0] == "b"){
+                outerPiece.style.filter = "invert()"
+            } else {
+                outerPiece.style.filter = "none"
+            }
+
+            outerPiece.appendChild(innerPiece)
+            containPiece.appendChild(outerPiece)
+            screenBoard.appendChild(containPiece)
+        }
+    }
+}
